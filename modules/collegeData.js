399f9edb4872654ccaed56878530a62c39b45961
aashish suwal <asuwal@myseneca.ca>
2024-07-20 19:33:41 -0400

    temp fix

--- modules/collegeData.js
+++ modules/collegeData.js
@@ -1,8 +1,7 @@
 const fs = require("fs");
-const path = require("path");
 
-class Data {
-    constructor(students, courses) {
+class Data{
+    constructor(students, courses){
         this.students = students;
         this.courses = courses;
     }
@@ -10,133 +9,136 @@ class Data {
 
 let dataCollection = null;
 
-// Function to initialize data from JSON files
 module.exports.initialize = function () {
-    return new Promise((resolve, reject) => {
-        // Read courses data
-        fs.readFile(path.join(__dirname, 'data', 'courses.json'), 'utf8', (err, courseData) => {
+    return new Promise( (resolve, reject) => {
+        fs.readFile('./data/courses.json','utf8', (err, courseData) => {
             if (err) {
-                reject("Unable to load courses");
-                return;
+                reject("Unable to load courses"); return;
             }
 
-            // Read students data
-            fs.readFile(path.join(__dirname, 'data', 'students.json'), 'utf8', (err, studentData) => {
+            fs.readFile('./data/students.json','utf8', (err, studentData) => {
                 if (err) {
-                    reject("Unable to load students");
-                    return;
+                    reject("Unable to load students"); return;
                 }
 
-                // Parse JSON data and create Data object
                 dataCollection = new Data(JSON.parse(studentData), JSON.parse(courseData));
-                resolve();
+                resolve(dataCollection.students.length);
             });
         });
     });
 }
 
-// Function to get all students
-module.exports.getAllStudents = function() {
-    return new Promise((resolve, reject) => {
-        if (dataCollection.students.length === 0) {
-            reject("No students found");
-            return;
+
+
+// function to get all students
+module.exports.getAllStudents = function(){
+    return new Promise((resolve,reject)=>{
+        if (dataCollection.students.length == 0) {
+            reject("no students"); return;
         }
+
         resolve(dataCollection.students);
-    });
+    })
 }
 
-// Function to get all courses
-module.exports.getCourses = function() {
-    return new Promise((resolve, reject) => {
-        if (dataCollection.courses.length === 0) {
-            reject("No courses found");
-            return;
+module.exports.getCourses = function(){
+   return new Promise((resolve,reject)=>{
+    if (dataCollection.courses.length == 0) {
+        reject("no course found"); return;
+    }
+    resolve(dataCollection.courses);
+   });
+};
+
+module.exports.getCourseById = function(num){
+    return new Promise((resolve,reject)=>{
+        var foundCourse = null;
+        for (let i = 0; i < dataCollection.courses.length; i++) {
+            if (dataCollection.courses[i].courseId == num) {
+                foundCourse = dataCollection.courses[i];
+            }
         }
-        resolve(dataCollection.courses);
+        if (!foundCourse) {
+            reject("no students for thius course"); return;
+        }
+        resolve(foundCourse);
     });
-}
+ };
 
-// Function to get a student by their number
 module.exports.getStudentByNum = function(num) {
-    return new Promise((resolve, reject) => {
-        const student = dataCollection.students.find(s => s.studentNum == num);
-        if (!student) {
-            reject("Student not found");
-            return;
+    return new Promise(function (resolve, reject) {
+        var foundStudent = null;
+        for (let i = 0; i < dataCollection.students.length; i++) {
+            if (dataCollection.students[i].studentNum == num) {
+                foundStudent = dataCollection.students[i];
+            }
         }
-        resolve(student);
+
+        if (!foundStudent) {
+            reject("no student by this numb"); return;
+        }
+
+        resolve(foundStudent);
     });
 };
 
-// Function to get students by course
-module.exports.getStudentsByCourse = function(course) {
-    return new Promise((resolve, reject) => {
-        const filteredStudents = dataCollection.students.filter(s => s.course === course);
-        if (filteredStudents.length === 0) {
-            reject("No students found for this course");
-            return;
+module.exports.getStudentsByCourse = function (course) {
+    return new Promise(function (resolve, reject) {
+        var filteredStudents = [];
+
+        for (let i = 0; i < dataCollection.students.length; i++) {
+            if (dataCollection.students[i].course == course) {
+                filteredStudents.push(dataCollection.students[i]);
+            }
         }
+
+        if (filteredStudents.length == 0) {
+            reject("no students on this course"); return;
+        }
+
         resolve(filteredStudents);
     });
 };
 
-// Function to add a new student
-module.exports.addStudent = function(requestBody) {
-    return new Promise((resolve, reject) => {
+module.exports.addStudent = function (requestBody) {
+    return new Promise(function (resolve, reject) {
         try {
             requestBody.studentNum = dataCollection.students.length + 1;
-            requestBody.TA = requestBody.TA === undefined ? false : true;
-            dataCollection.students.push(requestBody);
-
-            // Save updated students data to JSON file
-            fs.writeFile(path.join(__dirname, 'data', 'students.json'), JSON.stringify(dataCollection.students, null, 2), (err) => {
-                if (err) {
-                    reject("Unable to save student data");
-                    return;
-                }
-                resolve("Student Added");
-            });
+            if(requestBody.TA === undefined)
+                requestBody.TA = false;
+            else
+                requestBody.TA = true;
+                dataCollection.students.push(requestBody);
+            resolve("Student Added succesfully ");
         } catch (err) {
+            console.log(err)
             reject(err);
         }
     });
 };
 
-// Function to update an existing student
-module.exports.updateStudent = function(requestBody) {
-    return new Promise((resolve, reject) => {
+module.exports.updateStudent = function (requestBody) {
+    return new Promise(function (resolve, reject) {
         try {
-            const studentIndex = dataCollection.students.findIndex(s => s.studentNum == requestBody.studentNum);
-            if (studentIndex === -1) {
-                reject("Student not found");
-                return;
-            }
-            requestBody.TA = requestBody.TA === undefined ? false : true;
-            dataCollection.students[studentIndex] = requestBody;
-
-            // Save updated students data to JSON file
-            fs.writeFile(path.join(__dirname, 'data', 'students.json'), JSON.stringify(dataCollection.students, null, 2), (err) => {
-                if (err) {
-                    reject("Unable to save student data");
-                    return;
+            var studentIndex = null;
+            for (let i = 0; i < dataCollection.students.length; i++) {
+                if (dataCollection.students[i].studentNum == requestBody.studentNum) {
+                    studentIndex = i;
+                    break;
                 }
-                resolve("Student Updated");
-            });
+            }
+            if(studentIndex == null) {
+                reject("Invalid Student ID"); return;
+            }
+            if(requestBody.TA === undefined)
+                requestBody.TA = false;
+            else
+                requestBody.TA = true;
+                dataCollection.students[studentIndex] = requestBody;
+            resolve("Student Updated succesfully");
         } catch (err) {
+            console.log(err)
             reject(err);
         }
-    });
-};
-
-// Function to get a course by ID
-module.exports.getCourseById = function(id) {
-    return new Promise((resolve, reject) => {
-        const course = dataCollection.courses.find(c => c.courseId == id);
-        if (!course) {
-            reject("Course not found");
-            return;
-        }
-        resolve(course);
     });
 };
